285e6ddb8bdca4e3cb117fdf0710b9c52e1cc8fb
LeeThomas13 <lmtthomasritz@gmail.com>
2020-11-12 20:47:47 -0800

    got half of tests passing

---
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -0,0 +1,44 @@
+class Band:
+    def __init__(self, name, members=None):
+        self.name = name
+        self.members = members
+
+    def __str__(self):
+        return f"The band {self.name}"
+
+    def __repr__(self):
+        return f"Band instance. name={self.name}, members={self.members}"
+
+
+class Musician:
+    pass
+
+class Guitarist(Musician):
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__ (self):
+        return f"My name is {self.name} and I play guitar"
+
+    def __repr__(self):
+        return f"Guitarist instance. Name = {self.name}"
+
+class Bassist(Musician):
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__(self):
+        return f"My name is {self.name} and I play bass"
+
+    def __repr__(self):
+        return f"Bassist instance. Name = {self.name}"
+
+class Drummer(Musician):
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__(self):
+        return f"My name is {self.name} and I play drums"
+
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
